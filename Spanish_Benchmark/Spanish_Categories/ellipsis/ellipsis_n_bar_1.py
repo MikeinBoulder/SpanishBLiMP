7c34cd556a25049a9f26cafec356ca402c8911c4
ag-629 <andrew.gerlach@colorado.edu>
2020-04-23 21:18:32 -0600

    Added ellipsis 1

---
+++ Spanish_Benchmark/Spanish_Categories/ellipsis/ellipsis_n_bar_1.py
@@ -0,0 +1,105 @@
+from Spanish_Utils.randomize import choice
+from Spanish_Utils.vocab_sets import *
+from Spanish_Utils.string_utils import *
+import numpy as np
+import random
+import sys
+
+       # Subj1         V    D1  Obj   Adj    y Subj2 V   D2
+        # Juan          tuvo dos tazas verdes y Jane tuvo tres
+        # John  has(?)  had two green cups and Jane  has had three.
+        # Subj1 Aux1 V   D1  Adj   Obj  AND Subj2 Aux2 V   D2
+
+        #Subj1          V    D1  Obj   y Subj2 V   D2  Adj
+        # Juan          tuvo dos tazas y Jane tuvo tres verdes.
+        # John  has(?)  had two cups and Jane  has  had three green.
+        # Subj1 Aux1 V   D1  Obj  AND Subj2 Aux2 V   D2    Adj
+plural_dets_fem = [("dos", "tres"),
+               ("tres", "dos"),
+               ("quatro", "cinco"),
+               ("cinco", "quatro"),
+               ("varias", "unas cuantas"),
+               ("unas cuantas", "varios"),
+               ("muchos", "pocas"),
+               ("pocas", "muchas"),
+               ("muchas", "unas"),
+               ("tres", "más"),
+               ("tres", "menos"),
+               ("tres", "al menos tantas"),
+               ("tres", "casi la misma cantidad de")]
+plural_dets_masc = [("dos", "tres"),
+               ("tres", "dos"),
+               ("quatro", "cinco"),
+               ("cinco", "quatro"),
+               ("varios", "unos pocos"),
+               ("unos pocos", "varios"),
+               ("muchos", "pocos"),
+               ("pocos", "muchos"),
+               ("muchos", "unos"),
+               ("tres", "más"),
+               ("tres", "menos"),
+               ("tres", "al menos tantos"),
+               ("tres", "casi la misma cantidad de")]
+singular_dets_masc = [("uno", "tres"),
+                      ("uno", "dos"),
+                      ("uno", "cinco"),
+                      ("uno", "quatro"),
+                      ("uno", "varios"),
+                      ("uno", "few"),
+                      ("uno", "many"),
+                      ("uno", "muchos"),
+                      ("uno", "unos"),
+                      ("uno", "más"),
+                      ("uno", "casi la misma cantidad")]
+singular_dets_fem = [("una", "tres"),
+                     ("una", "dos"),
+                     ("una", "cinco"),
+                     ("una", "quatro"),
+                     ("una", "varias"),
+                     ("una", "pocas"),
+                     ("una", "muchas"),
+                     ("una", "unas"),
+                     ("una", "más"),
+                     ("una", "casi la misma cantidad")]
+safe_objs = np.setdiff1d(all_nominals, all_proper_nouns)
+
+def sample(iter, out):
+    for i in range(iter):
+        V = choice(past_pret_3s)
+        Subj1 = choice(all_proper_nouns)
+        Subj2 = choice(all_proper_nouns, Subj1)
+        if choice([True,False]):
+            #Plural Obj
+            if choice([True,False]):
+                Obj = choice(all_inanimate_pl_nouns_f)
+                Adj = choice(np.union1d(all_adj_pl_fem,all_adj_pl_neuter))
+                D1, D2 = random.choice(plural_dets_fem)
+            else:
+                Obj = choice(all_inanimate_pl_nouns_m)
+                Adj = choice(np.union1d(all_adj_pl_masc, all_adj_pl_neuter))
+                D1, D2 = random.choice(plural_dets_masc)
+        else:
+            #Singular Obj
+            if choice([True,False]):
+                Obj = choice(all_inanimate_sg_nouns_f)
+                Adj = choice(np.union1d(all_adj_sg_fem, all_adj_sg_neuter))
+                D1, D2 = random.choice(singular_dets_fem)
+            else:
+                Obj = choice(all_inanimate_sg_nouns_m)
+                Adj = choice(np.union1d(all_adj_sg_masc,all_adj_sg_neuter))
+                D1, D2 = random.choice(singular_dets_masc)
+
+        data = {
+            "sentence_good": "%s %s %s %s %s %s %s %s %s." % (Subj1, V, D1, Obj, Adj, 'y', Subj2, V, D2),
+            "sentence_bad": "%s %s %s %s %s %s %s %s %s." % (Subj1, V, D1, Obj, 'y', Subj2, V, D2, Adj),
+        }
+        out.write(str(data) + '\n')
+try:
+    iter = int(sys.argv[1])
+    out = open(sys.argv[2], 'w')
+    sample(iter, out)
+except IndexError:
+    print("To run this file:\npython ellipsis_n_bar_1.py <# sentences> <output path>")
+    sys.exit()
+
+ 
--- Spanish_Benchmark/Spanish_Categories/ellipsis/ellipsis_n_bar_2.py
+++ Spanish_Benchmark/Spanish_Categories/ellipsis/ellipsis_n_bar_2.py
@@ -1,14 +1,14 @@
-from Spanish_Utils.randomize import choice
-from Spanish_Utils.vocab_sets import *
-from Spanish_Utils.string_utils import *
-
-
+from utils import data_generator
+from utils.constituent_building import *
+from utils.conjugate import *
+from utils.randomize import choice
+from utils.vocab_sets import *
 
 class AgreementGenerator(data_generator.BenchmarkGenerator):
     def __init__(self):
         super().__init__(field="syntax",
                          linguistics="ellipsis",
-                         uid="ellipsis_n_bar_1",
+                         uid="ellipsis_n_bar_2",
                          simple_lm_method=True,
                          one_prefix_method=False,
                          two_prefix_method=False,
@@ -44,13 +44,15 @@ class AgreementGenerator(data_generator.BenchmarkGenerator):
                             ("one", "more"),
                             ("one", "at least as many")
                             ]
+
         self.safe_objs = np.setdiff1d(all_nominals, all_proper_names)
 
     def sample(self):
-        # John  has  had two green cups and Jane  has had three.
-        # Subj1 Aux1 V   D1  Adj   Obj  AND Subj2 Aux2 V   D2
-        # John  has  had two cups and Jane  has  had three green.
-        # Subj1 Aux1 V   D1  Obj  AND Subj2 Aux2 V   D2    Adj
+        # John  has  had two cups and Jane  has  had three green cups.
+        # Subj1 Aux1 V   D1  Obj  AND Subj2 Aux2 V   D2    Adj2  Obj
+        # John  has  had two red  cups and Jane  has  had three green.
+        # Subj1 Aux1 V   D1  Adj1 Obj  AND Subj2 Aux2 V   D2    Adj2
+
         V = choice(self.safe_verbs)
         Subj1 = choice(get_matches_of(V, "arg_1", all_nominals))
         Subj2 = choice(get_matches_of(V, "arg_1", all_nominals), avoid=Subj1)
@@ -62,16 +64,16 @@ class AgreementGenerator(data_generator.BenchmarkGenerator):
         else:
             Aux2 = return_aux(V, subj=Subj2)
         Obj = choice(get_matches_of(V, "arg_2", get_all("mass", "0", self.safe_objs)))
-        Adj = choice(get_matched_by(Obj, "arg_1", all_adjectives))
+        Adj1 = choice(get_matched_by(Obj, "arg_1", all_adjectives))
+        Adj2 = choice(get_matched_by(Obj, "arg_1", all_adjectives), avoid=Adj1)
         if Obj["pl"] == "0":
             (D1, D2) = random.choice(self.singular_dets)
         else:
             (D1, D2) = random.choice(self.plural_dets)
 
         data = {
-            "sentence_good": "%s %s %s %s %s %s and %s %s %s %s." % (Subj1[0], Aux1[0], V[0], D1, Adj[0], Obj[0], Subj2[0], Aux2[0], V[0], D2),
-            "sentence_bad": "%s %s %s %s %s and %s %s %s %s %s." % (Subj1[0], Aux1[0], V[0], D1, Obj[0], Subj2[0], Aux2[0], V[0], D2, Adj[0]),
-        }
+            "sentence_good": "%s %s %s %s %s and %s %s %s %s %s %s." % (Subj1[0], Aux1[0], V[0], D1, Obj[0], Subj2[0], Aux2[0], V[0], D2, Adj2[0], Obj[0]),
+            "sentence_bad": "%s %s %s %s %s %s and %s %s %s %s %s." % (Subj1[0], Aux1[0], V[0], D1, Adj1[0], Obj[0], Subj2[0], Aux2[0], V[0], D2, Adj2[0]),        }
         return data, data["sentence_good"]
 
 generator = AgreementGenerator()
